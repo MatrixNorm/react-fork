3324b8c7dadb90e8e9f38df684433440b87255f4
Ubuntu <ubuntu@nodejs.lxd>
2023-08-10 10:33:47 +0000

    *

--- packages/matrixnorm/index.js
+++ packages/matrixnorm/index.js
@@ -1,74 +1,122 @@
-import type { Fiber } from 'react-reconciler/src/ReactInternalTypes';
+/**
+ * @flow
+ */
 
-export const getFiberType = fiber => {
-  switch (fiber.tag) {
-    case 0: {
-      return `func <${fiber.type.name}>`;
-    }
-    case 2: {
-      return `indeterminate <${fiber.type.name}>`;
-    }
-    case 3: {
-      return 'hostRoot';
-    }
-    case 14: {
-      return `memo type:${fiber.elementType.type.name}`
-    }
-    case 15: {
-      return `simpleMemo type:${fiber.type.name}`
-    }
-    default:
-      return fiber.type;
+import type { Fiber, FiberRoot } from 'react-reconciler/src/ReactInternalTypes';
+import { fiberInfoShort } from './print'
+
+export * from './print';
+export { fiberTreeToXMLv2 as fiberTreeToXML } from './fiberTree';
+export { fiberTreeToXMLv4 as fiberTreeToXML2 } from './fiberTree2';
+
+type Phase = "enter" | "leave" | "leaf";
+type FiberTreeGenerator = Generator<[Phase, Fiber], void, void>
+
+function* iterFiberTree(node: Fiber): FiberTreeGenerator {
+  if (node.child) {
+    yield ["enter", node];
+    yield* iterFiberTree(node.child);
+    yield ["leave", node];
+  } else {
+    yield ["leaf", node];
   }
-};
-
-export const fiberInfo = (fiber: $ReadOnly<Fiber>): void => {
-  return fiber ? `Fib <tag=${fiber.tag} ${getFiberType(fiber)}>` : 'Fib <NULL>';
-};
-
-export const elementInfo = element => {
-  if (!element) return null;
-
-  if (Array.isArray(element)) {
-    return element.map(el => elementInfo(el));
+  if (node.sibling) {
+    yield* iterFiberTree(node.sibling);
   }
+}
 
-  const _element = { ...element };
-  _element._owner = fiberInfo(element._owner);
-  return _element;
-};
+function* iterFiberTreeX(node: Fiber): FiberTreeGenerator {
+  if (node.child) {
+    yield ["enter", node];
+    for (let res of iterFiberTreeX(node.child)) {
+      yield res;
+    }
+    yield ["leave", node];
+  } else {
+    yield ["leaf", node];
+  }
+  if (node.sibling) {
+    for (let res of iterFiberTreeX(node.sibling)) {
+      yield res;
+    }
+  }
+}
 
-export const getStackTrace = depth => {
+function* iterFiberTreeY(node: Fiber): FiberTreeGenerator {
+  while (node) {
+    if (node.child) {
+      yield ["enter", node];
+      for (let res of iterFiberTreeY(node.child)) {
+        yield res;
+      }
+      yield ["leave", node];
+    } else {
+      yield ["leaf", node];
+    }
+
+    if (node.sibling) {
+      node = node.sibling;
+    } else {
+      break;
+    }
+  };
+}
+
+const fiberTreeToXMLWithGenerator = (generator: Fiber => FiberTreeGenerator) => (startNode: Fiber): string => {
+  const tab = "  ";
+  let result = "";
+  let d = -1;
+
+  for (let [phase, fiber] of generator(startNode)) {
+    const fibInfo = fiberInfoShort(fiber);
+    if (phase === "enter") {
+      d++;
+      result += `${tab.repeat(d)}<${fibInfo}>\n`;
+    } else if (phase === "leave") {
+      result += `${tab.repeat(d)}</${fibInfo}>\n`;
+      d--;
+    } else {
+      d++;
+      result += `${tab.repeat(d)}<${fibInfo} />\n`;
+      d--;
+    }
+  }
+  return result;
+}
+
+export const fiberTreeToXML3: Fiber => string = fiberTreeToXMLWithGenerator(iterFiberTree);
+export const fiberTreeToXML3X: Fiber => string = fiberTreeToXMLWithGenerator(iterFiberTreeX);
+export const fiberTreeToXML3Y: Fiber => string = fiberTreeToXMLWithGenerator(iterFiberTreeY);
+
+
+
+// const fiberTreeToObject2 = (wipNode: Fiber, curNode: Fiber) => {
+//   if (wipNode === curNode) {
+//     let { child: firstChild } = wipNode;
+//     if (firstChild) {
+//       let children = __getAllChildrenOfFiber(wipNode);
+//       return { [`(!)${fiberInfo(wipNode)}`]: children.map(fiberTreeToObject) };
+//     } else {
+//       return `(!)${fiberInfo(wipNode)}`;
+//     }
+//   } else {
+//     let { child: wipFirstChild } = wipNode;
+//     let { child: curFirstChild } = curNode;
+//     // XXX
+//   }
+// }
+
+export const getStackTrace = (depth: number): string => {
   let obj = {};
   Error.captureStackTrace(obj, getStackTrace);
-
   let stackStr = obj.stack
     .split('\n')
     .slice(1, depth + 1)
-    // .map(frame => {
-    //   console.log(frame)
-    //   let frameData = frame.trim().split(' ');
-    //   let functionName = frameData[1];
-    //   let filePath = frameData[2].split('/');
-    //   let fileName = filePath[filePath.length - 1];
-    //   fileName = fileName.substring(0, fileName.lastIndexOf(':'));
-    //   return {
-    //     functionName,
-    //     fileName,
-    //   };
-    // })
+    .map(frame => {
+      return frame.trim().slice(3).replace('/home/ubuntu/projects/react-fork/packages/', '');
+    })
     // .map(({functionName, fileName}) => `${functionName}:: ${fileName}`)
     .join('\n');
 
   return '\n\n' + stackStr;
-};
-
-export const log = namespace => {
-  const _NAMESPACES = window.__MATRIX_NORM_LOG_NAMESPACES;
-
-  if (_NAMESPACES && _NAMESPACES.includes(namespace)) {
-    return console.log;
-  } else {
-    return () => { };
-  }
 };
