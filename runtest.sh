e370105e982e500824103ecbb17efd50f2b1529e
Ubuntu <ubuntu@nodejs.lxd>
2023-08-19 19:15:09 +0000

    *

--- runtest.sh
+++ runtest.sh
@@ -1,5 +1,4 @@
 NODE_ENV=development \
 RELEASE_CHANNEL=experimental \
 compactConsole=false \
-node --inspect=127.0.0.1:9230 \
-./scripts/jest/jest.js --config ./scripts/jest/config.source.js "$@"
+node ./scripts/jest/jest.js --config ./scripts/jest/config.source.js "$@"
